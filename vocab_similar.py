fea40b879a6ead74c6d2d6010c645547a3827214
Ishita <ishitagops12@gmail.com>
2022-02-13 22:00:35 +0530

    vocab similarity using spacy

--- vocab_similar.py
+++ vocab_similar.py
@@ -6,11 +6,11 @@ def startpy():
 
    nlp = spacy.load('en_core_web_sm') 
 
-   text = "I love maggi"
+   text = "I love paneer"
 
    doc = nlp(text)
 
-   print("hash value: " , nlp.vocab.strings["maggi"])
+   print("hash value: " , nlp.vocab.strings["paneer"])
    print("string value: ", nlp.vocab.strings[3197928453018144401])
    
 if __name__ == '__main__':
